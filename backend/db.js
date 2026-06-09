5f40c7982ee3196e6b2f11147bf065bdcbccd9eb
Varunreddy-45 <kopperavarunreddy@gmail.com>
2026-06-09 14:32:06 +0530

    Configure Aiven MySQL connection

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,22 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "interview_management"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
-
   if (err) {
-    console.log("Database Connection Failed");
+    console.log("Database Connection Failed", err);
   } else {
     console.log("MySQL Connected");
   }
-
 });
 
 module.exports = db;
